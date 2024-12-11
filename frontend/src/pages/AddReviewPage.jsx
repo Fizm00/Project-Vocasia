105a4d25c8e0d65403b41eebc1ca9fd29a3541f8
amlly38 <amaliyahamel0308@gmail.com>
2024-12-12 04:05:44 +0700

    refactor: clean up code by removing unnecessary comments

--- frontend/src/pages/AddReviewPage.jsx
+++ frontend/src/pages/AddReviewPage.jsx
@@ -4,12 +4,10 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const AddReview = () => {
-  // State untuk rating dan komentar
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  // Data kost yang sedang disewa
   const rentalDetails = {
     image: 'https://i.pinimg.com/736x/3c/32/cd/3c32cdbe6323e085400f43447133169d.jpg',
     startDate: '2023-08-01',
@@ -22,30 +20,28 @@ const AddReview = () => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('id-ID', options); // Menggunakan format Indonesia
+    return date.toLocaleDateString('id-ID', options); 
   };
 
-  // Menangani perubahan input komentar
+  
   const handleCommentChange = (e) => {
     setComment(e.target.value);
   };
 
-  // Menangani perubahan rating
+  
   const handleRatingClick = (index) => {
     setRating(index);
   };
 
-  // Menangani submit form review
+  
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Validasi sebelum submit
     if (rating === 0 || comment.trim() === '') {
       alert('Mohon beri rating dan komentar!');
       return;
     }
 
-    // Jika validasi sukses, tampilkan alert (seharusnya disimpan ke backend)
     setSubmitted(true);
     alert('Review berhasil ditambahkan!');
     console.log({ rating, comment });
@@ -57,7 +53,7 @@ const AddReview = () => {
       <div className="min-h-screen flex justify-center py-6 px-4 sm:px-6 lg:px-8">
         <div className="w-full md:w-3/4 lg:w-2/3 bg-white rounded-lg border border-5 border-gray-200 p-8 mb-10">
           <h1 className="text-left text-md md:text-xl font-bold text-gray-800 mb-4">Yuk kasih review kost yang kamu sewa!</h1>
-
+          {/* Display Kost */}
           <div className="flex flex-col md:flex-row rental-details mt-2 space-y-4 md:space-y-0">
             <div className='w-full md:w-1/3'>
               <img src={rentalDetails.image} alt="Kost" className="w-full h-48 object-cover rounded-lg" />
