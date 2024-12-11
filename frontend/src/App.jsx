c0211ebaea4ce6622b2431f41631c02a30ed4524
unknown <herofirefox10@gmail.com>
2024-12-11 15:36:48 +0700

    masukkan data ke localstorage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import './App.css';
-import './index.css';
-import Register from './pages/Register.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import ForgotPassword from './pages/ForgotPassword';
-import Home from './pages/Home';
-import SearchResultPage from './pages/SearchResultPage.jsx';
-import DetailPage from './pages/DetailPage.jsx';
-import RentalHistory from './pages/RentalHistory.jsx';
-import About from './pages/About.jsx';
-import Payment from './pages/Payment.jsx';
-import Booking from './pages/Booking.jsx';
-import SuccessBook from './pages/SuccessBook.jsx'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+// import React from "react";
+import "./App.css";
+import "./index.css";
+import Register from "./pages/Register.jsx";
+
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import SearchResultPage from "./pages/SearchResultPage.jsx";
+import DetailPage from "./pages/DetailPage.jsx";
+import RentalHistory from "./pages/RentalHistory.jsx";
+import About from "./pages/About.jsx";
+import Payment from "./pages/Payment.jsx";
+import Booking from "./pages/Booking.jsx";
+import SuccessBook from "./pages/SuccessBook.jsx";
 
 function App() {
   return (
@@ -24,12 +25,16 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/search" element={<SearchResultPage />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/detail/:id" element={<DetailPage />} key="detail"></Route>
+          <Route
+            path="/detail/:id"
+            element={<DetailPage />}
+            key="detail"
+          ></Route>
           <Route path="/riwayat-sewa" element={<RentalHistory />} />
           <Route path="/about" element={<About />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/booking" element={<Booking />} />
-          <Route path="/success-book" element={<SuccessBook />} /> 
+          <Route path="/success-book" element={<SuccessBook />} />
         </Routes>
       </Router>
     </div>
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,14 +1,19 @@
 import axiosInstance from "../config/axiosInstance";
+// import { useState, useEffect } from "react";
 
 const loginUser = async (email, password) => {
   try {
-    const response = await axiosInstance.post("/login", {
-      email,
-      password,
-    });
-    return response.data;
+    const response = await axiosInstance.post("/login", { email, password });
+
+    // Save to localStorage
+    localStorage.setItem("token", response.data.data.token);
+    localStorage.setItem("email", JSON.stringify(response.data.data.email));
+    localStorage.setItem("name", JSON.stringify(response.data.data.name));
+    console.log(response.data.data);
+
+    return response.data.data; // Return the response data
   } catch (error) {
-    throw error.response.data;
+    throw error.response?.data || "An error occurred";
   }
 };
 
