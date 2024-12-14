d1c7d26988dd528b6a7eef6030f7682e997f64b3
Firza Himawan <firzahimawan.2022@student.uny.ac.id>
2024-12-14 15:36:01 +0700

    feat: add backend files

---
+++ backend/middleware/authenticateJWT.js
@@ -0,0 +1,34 @@
+const redisClient = require("../config/redis");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const authenticateJWT = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied: No token provided." });
+  }
+
+  try {
+    // Periksa apakah Redis Client aktif
+    if (!redisClient.isOpen) {
+      console.error("Redis client is not connected");
+      await redisClient.connect();
+    }
+
+    const checkToken = await redisClient.get(`BLACKLIST_TOKEN:${token}`);
+    if (checkToken) {
+      return res
+        .status(403)
+        .json({ message: "Warning: Token has been blacklisted" });
+    }
+    next(); // Token valid
+  } catch (error) {
+    console.error("Redis Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = authenticateJWT;
