4663a0fc38e6168033fd8d0440f7030868909f20
unknown <herofirefox10@gmail.com>
2024-12-15 20:55:28 +0700

    penamabahan api property

---
+++ frontend/src/api/property.js
@@ -0,0 +1,25 @@
+import axiosInstance from "../config/axiosInstance";
+
+export const getPropertyById = async (id) => {
+  try {
+    const response = axiosInstance.get(`/property/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(
+      " Error Fetching data by id| getPropertyById - error:" + error
+    );
+    throw error.response;
+  }
+};
+
+export const getProperty = async () => {
+  try {
+    const response = await axiosInstance.get("/properties");
+    return response.data;
+  } catch (error) {
+    console.error(" Error Fetching data| getProperty - error:" + error);
+    throw error.response;
+  }
+};
+
+// export default getPropertyById;
