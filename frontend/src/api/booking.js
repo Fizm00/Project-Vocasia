548d6b8e76a04d1a3823bbc77b9cb64526711c52
unknown <herofirefox10@gmail.com>
2024-12-17 16:58:27 +0700

    penambahan api booking

---
+++ frontend/src/api/booking.js
@@ -0,0 +1,51 @@
+import axiosInstance from "../config/axiosInstance";
+
+const getBookings = async (token) => {
+  try {
+    const response = await axiosInstance(
+      `${import.meta.env.VITE_BASE_API_URL}/bookings`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    localStorage.setItem("token", response.data.data.token);
+    localStorage.setItem("email", JSON.stringify(response.data.data.email));
+    localStorage.setItem("name", JSON.stringify(response.data.data.name));
+    localStorage.setItem("user", JSON.stringify(response.data.data.user_id));
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error.response?.data || "An error occurred";
+  }
+};
+
+const getBookingsById = async (id) => {
+  try {
+    const response = await axiosInstance.get(`/booking/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || "An error occurred";
+  }
+};
+
+const createBooking = async (data) => {
+  const { user_id, property_id, start_date, end_date } = data;
+
+  // Validasi data
+  if (!user_id || !property_id || !start_date || !end_date) {
+    throw new Error("Semua field harus diisi!");
+  }
+  try {
+    const response = await axiosInstance.post("/booking", data);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || "An error occurred";
+  }
+};
+
+export { getBookings, getBookingsById, createBooking };
