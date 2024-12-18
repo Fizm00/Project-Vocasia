750433baa4c12648607b803c4195b15111de874c
unknown <herofirefox10@gmail.com>
2024-12-18 14:46:56 +0700

    data tampil tanpa gambar | gambar error

--- frontend/src/components/RentalHistoryList.jsx
+++ frontend/src/components/RentalHistoryList.jsx
@@ -69,74 +69,18 @@ const RentalHistoryList = ({ activeTab }) => {
     fetchRentals();
   }, []);
 
-  // const rentals = [
-  //   {
-  //     id: 1,
-  //     name: "Kost Trinanda",
-  //     startDate: "12 Mar 2023",
-  //     endDate: "12 Apr 2023",
-  //     duration: "1 Bulan",
-  //     price: "Rp 1.500.000 / bulan",
-  //     image: "/1-kostImage.png",
-  //     status: "Proses",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Kost Sultan Kencana",
-  //     startDate: "15 Mar 2023",
-  //     endDate: "15 Apr 2023",
-  //     duration: "1 Bulan",
-  //     price: "Rp 1.200.000 / bulan",
-  //     image: "/2-kostImage.png",
-  //     status: "Disetujui",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Kost Rahayu",
-  //     startDate: "20 Mar 2023",
-  //     endDate: "20 Apr 2023",
-  //     duration: "1 Bulan",
-  //     price: "Rp 2.000.000 / bulan",
-  //     image: "/3-kostImage.png",
-  //     status: "Ditolak",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Kost Keraton",
-  //     startDate: "22 Mar 2023",
-  //     endDate: "22 May 2023",
-  //     duration: "2 Bulan",
-  //     price: "Rp 1.000.000 / bulan",
-  //     image: "/4-kostImage.png",
-  //     status: "Proses",
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Kost Harmoni",
-  //     startDate: "25 Mar 2023",
-  //     endDate: "25 Apr 2023",
-  //     duration: "1 Bulan",
-  //     price: "Rp 1.800.000 / bulan",
-  //     image: "/5-kostImage.png",
-  //     status: "Disetujui",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "Kost Eden",
-  //     startDate: "30 Mar 2023",
-  //     endDate: "30 Apr 2023",
-  //     duration: "1 Bulan",
-  //     price: "Rp 1.700.000 / bulan",
-  //     image: "/6-kostImage.png",
-  //     status: "Ditolak",
-  //   },
-  // ];
+  // const filteredRentals = rentals.filter((rental) =>
+  //   activeTab === "proses"
+  //     ? rental.status === "Proses"
+  //     : rental.status === "Disetujui" || rental.status === "Ditolak"
+  // );
 
-  const filteredRentals = rentals.filter((rental) =>
-    activeTab === "proses"
-      ? rental.status === "Proses"
-      : rental.status === "Disetujui" || rental.status === "Ditolak"
-  );
+  const getFilteredRentals = () => {
+    if (activeTab === "proses")
+      return rentals.filter((r) => r.status === "Proses");
+    return rentals.filter((r) => r.status !== "Proses");
+  };
+  const filteredRentals = getFilteredRentals();
 
   const handleNavigateToPayment = (rentalId) => {
     navigate(`/payment/${rentalId}`);
@@ -164,13 +108,13 @@ const RentalHistoryList = ({ activeTab }) => {
 
   return (
     <div className="space-y-4">
-      {rentals.map((rental) => (
+      {filteredRentals.map((rental) => (
         <div
           key={rental._id}
           className="flex flex-col sm:flex-row items-center p-4 bg-white border rounded-md shadow-sm transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
         >
           <img
-            src={rental.image}
+            // src={import+rental.images[0]}
             alt={rental.name}
             className="w-24 h-20 sm:w-32 sm:h-24 object-cover rounded-lg mb-3 sm:mb-0 sm:mr-6"
           />
@@ -179,10 +123,12 @@ const RentalHistoryList = ({ activeTab }) => {
               {rental.name}
             </h3>
             <p className="text-gray-600">
-              {rental.startDate} - {rental.endDate}
+              {rental.start_date} - {rental.end_date}
+              star end date
+              {/* {rental.startDate} - {rental.endDate} */}
             </p>
-            <p className="text-gray-600">Durasi: {rental.duration}</p>
-            <p className="font-semibold text-[#193F3D]">{rental.price}</p>
+            {/* <p className="text-gray-600">Durasi: {rental.duration}</p> */}
+            <p className="font-semibold text-[#193F3D]">{rental.total_price}</p>
           </div>
           <button
             className={`w-full sm:w-auto px-6 py-1 text-xs font-medium rounded-full transition-all duration-300 ${
@@ -194,7 +140,7 @@ const RentalHistoryList = ({ activeTab }) => {
             }`}
             onClick={() =>
               rental.status === "Disetujui" &&
-              handleNavigateToPayment(rental.id)
+              handleNavigateToPayment(rental._id)
             }
           >
             {rental.status}
