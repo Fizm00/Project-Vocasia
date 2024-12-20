ddb8347300d32a11ffbf5a8a92868270c953a4e9
amlly38 <amaliyahamel0308@gmail.com>
2024-12-20 12:25:37 +0700

    feat: add fetch on dashboard page for owner

--- frontend/src/components/ProfilePage/Menu.jsx
+++ frontend/src/components/ProfilePage/Menu.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHistory, FaCreditCard, FaUserPlus } from 'react-icons/fa';
+import { FaHistory, FaUserPlus, FaHome } from 'react-icons/fa';
+import { MdDashboardCustomize } from "react-icons/md";
+import { IoPersonCircle } from "react-icons/io5";
 
 const Menu = ({ formData }) => {
     return (
@@ -12,6 +14,7 @@ const Menu = ({ formData }) => {
                         alt="User"
                         className="rounded-full w-12 h-12 mr-2 object-cover"
                     /> */}
+                    <IoPersonCircle size={35} className='mr-2 text-darkGreen'/>
                     <div className="text-lg font-bold">{formData.name}</div>
                 </div>
                 <hr className="w-full my-4 border-t-1 border-gray-300" />
@@ -20,19 +23,28 @@ const Menu = ({ formData }) => {
                         <FaHistory className="mr-2" /> 
                         <p>Riwayat Pengajuan Sewa</p>
                     </Link>
-                    <Link to="/transactions-history" className="flex items-center text-gray-800">
-                        <FaCreditCard className="mr-2" />
-                        <p>Riwayat Transaksi</p>
+                    <Link to="/" className="flex items-center text-gray-800">
+                        <FaHome className="mr-2" />
+                        <p>Beranda</p>
                     </Link>
                 </div>
             </div>
-            <div className="mt-8 p-6 bg-white rounded-lg shadow-lg border">
+            <div className="mt-4 p-6 bg-white rounded-lg shadow-lg border">
                 <p className="mb-2 text-lg font-bold">Kamu Punya Kost?</p>
                 <hr className="w-full my-4 border-t-1 border-gray-300" />
-                <p className="text-md mb-4">Upgrade akun kamu menjadi pemilik kost dan bergabung bersama kami!</p>
-                <Link to="/verifikasi" className="flex justify-center items-center font-semibold bg-darkGreen py-2 px-4 rounded-lg text-white hover:bg-white hover:text-darkGreen hover:border hover:border-darkGreen">
+                <p className="text-md mb-4">Upgrade akun kamu menjadi pemilik kost!</p>
+                <Link to="/verification" className="flex justify-center items-center font-semibold bg-darkGreen py-2 px-4 rounded-lg text-white hover:bg-white hover:text-darkGreen hover:border hover:border-darkGreen">
                     <FaUserPlus className="mr-2" />
                     <p>Daftar Sekarang</p>
+                </Link>
+            </div>
+            <div className="mt-4 p-6 bg-white rounded-lg shadow-lg border">
+                <p className="mb-2 text-lg font-bold">Kelola Kost Kamu Disini!</p>
+                <hr className="w-full my-4 border-t-1 border-gray-300" />
+                <p className="text-md mb-4">Kelola Kost dengan praktis, semua di satu tempat!</p>
+                <Link to="/dashboard" className="flex justify-center items-center font-semibold bg-darkGreen py-2 px-4 rounded-lg text-white hover:bg-white hover:text-darkGreen hover:border hover:border-darkGreen">
+                    <MdDashboardCustomize className="mr-2" />
+                    <p>Dashboard</p>
                 </Link>
             </div>
         </div>
